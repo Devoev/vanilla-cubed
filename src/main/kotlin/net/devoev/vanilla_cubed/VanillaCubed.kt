dafb22883d5b4bbe1cf8a095c010617f5c564b78
Devin Balian <devinbalian@gmail.com>
2022-03-23 19:50:45 +0100

    Sample item VanillaCubed.kt entry object

--- src/main/kotlin/net/devoev/vanilla_cubed/VanillaCubed.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/VanillaCubed.kt
@@ -1,13 +1,31 @@
 package net.devoev.vanilla_cubed
 
-// For support join https://discord.gg/v6v4pMv
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.block.Block
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
 
-@Suppress("unused")
-fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
+object VanillaCubed : ModInitializer {
 
-    println("Hello Fabric world!")
+    private const val MOD_ID = "vanilla_cubed"
+
+    private val items = mapOf(
+        "donut" to Item(FabricItemSettings().group(ItemGroup.FOOD))
+    )
+
+    private val blocks = mapOf<String, Block>()
+
+    override fun onInitialize() {
+        register(Registry.ITEM, items)
+        register(Registry.BLOCK, blocks)
+    }
+
+    private fun <V, T : V> register(type: Registry<V>, entries: Map<String, T>) {
+        for (e in entries)
+            Registry.register(type, Identifier(MOD_ID, e.key), e.value)
+    }
 }
 
