e0141440ee65b23fb4b2713cfd346e256b2dc8ae
Devin Balian <devinbalian@gmail.com>
2022-04-02 13:54:21 +0200

    WIP AncientGoldMixin.

---
+++ src/main/java/net/devoev/vanilla_cubed/mixin/AncientGoldMixin.java
@@ -0,0 +1,29 @@
+package net.devoev.vanilla_cubed.mixin;
+
+import net.devoev.vanilla_cubed.item.ModItems;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ExperienceOrbEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map.Entry;
+
+/**
+ * A mixin for ancient gold armor and tools.
+ */
+@Mixin(ExperienceOrbEntity.class)
+public class AncientGoldMixin {
+
+    @Inject(method = "repairPlayerGears", at = @At(value = "HEAD"))
+    private void repairPlayerGears(PlayerEntity player, int amount, CallbackInfoReturnable info) {
+        Entry<EquipmentSlot, ItemStack> entry = EnchantmentHelper.chooseEquipmentWith(Enchantments.MENDING, player, ItemStack::isDamaged);
+        if (entry != null && entry.getValue().getItem().equals(ModItems.INSTANCE.getANCIENT_GOLD_PICKAXE()))
+            System.out.println("Found ancient gold pickaxe!");
+    }
+}
