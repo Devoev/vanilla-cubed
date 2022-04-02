d5e58c7c4ff44b236dfecd702e81ee7902cbf75d
Devin Balian <devinbalian@gmail.com>
2022-04-02 12:38:21 +0200

    Removed ExampleMixin.java. Updated AmethystCrystalMixin.java, to use Kotlin ModItems.

--- src/main/java/net/devoev/vanilla_cubed/mixin/AmethystCrystalMixin.java
+++ src/main/java/net/devoev/vanilla_cubed/mixin/AmethystCrystalMixin.java
@@ -1,13 +1,11 @@
 package net.devoev.vanilla_cubed.mixin;
 
+import net.devoev.vanilla_cubed.item.ModItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LightningEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,16 +22,11 @@ public class AmethystCrystalMixin {
      */
     @Inject(method = "onStruckByLightning", at = @At("HEAD"))
     private void onStruckByLightning(ServerWorld world, LightningEntity lightning, CallbackInfo info) {
+        if (!( (Object) this instanceof ItemEntity item && item.getStack().getItem().equals(ModItems.INSTANCE.getAMETHYST_CRYSTAL()))) return;
 
-        if ((Object) this instanceof ItemEntity item) {
-            Item crystal = Registry.ITEM.get(new Identifier("vanilla_cubed", "amethyst_crystal"));
-            Item charged_crystal = Registry.ITEM.get(new Identifier("vanilla_cubed", "amethyst_crystal_charged"));
-            if (!item.getStack().getItem().equals(crystal)) return;
-
-            item.setStack(new ItemStack(charged_crystal, item.getStack().getCount()));
-            item.setInvulnerable(true);
-            item.setInvisible(false);
-            item.setOnFire(false);
-        }
+        item.setStack(new ItemStack(ModItems.INSTANCE.getCHARGED_AMETHYST_CRYSTAL(), item.getStack().getCount()));
+        item.setInvulnerable(true);
+        item.setInvisible(false);
+        item.setOnFire(false);
     }
 }
