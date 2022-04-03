94bdddb8ba8cec1a1c810872b024933a13ddf2f5
Devin Balian <devinbalian@gmail.com>
2022-04-03 14:34:00 +0200

    Updated ModWorldGeneration.kt (WIP)

--- src/main/kotlin/net/devoev/vanilla_cubed/world/gen/ModWorldGeneration.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/world/gen/ModWorldGeneration.kt
@@ -2,18 +2,33 @@ package net.devoev.vanilla_cubed.world.gen
 
 import net.devoev.vanilla_cubed.world.feature.ModPlacedFeatures
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
 import net.minecraft.util.registry.Registry
 import net.minecraft.util.registry.RegistryKey
 import net.minecraft.world.gen.GenerationStep
+import net.minecraft.world.gen.feature.PlacedFeature
+import java.util.function.Predicate
 
 /**
  * All world generation features.
  */
-object ModWorldGeneration {
+object ModWorldGeneration : MutableList<GenerationFeature> by mutableListOf() {
 
-    fun init() {
-        BiomeModifications.addFeature(BiomeSelectors.foundInTheEnd(),
-            GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, ModPlacedFeatures[ModPlacedFeatures.ENDERITE_ORE]))
+    val ENDERITE_ORE = create(BiomeSelectors.foundInTheEnd(), GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.ENDERITE_ORE)
+
+    fun create(value: GenerationFeature): GenerationFeature {
+        add(value)
+        return value
     }
+
+    fun create(biomeSelector: Predicate<BiomeSelectionContext>, step: GenerationStep.Feature, feature: PlacedFeature): GenerationFeature
+        = create(Triple(biomeSelector, step, RegistryKey.of(Registry.PLACED_FEATURE_KEY, ModPlacedFeatures[feature])))
+
+    /**
+     * Initializes all world generation features.
+     */
+    fun init() = forEach { BiomeModifications.addFeature(it.first, it.second, it.third) }
 }
+
+typealias GenerationFeature = Triple<Predicate<BiomeSelectionContext>, GenerationStep.Feature, RegistryKey<PlacedFeature>>
