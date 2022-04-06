2f3c8282ee13680b7509248b36458be7f775f42a
Devin Balian <devinbalian@gmail.com>
2022-04-06 14:41:43 +0200

    Updated EnchantmentHelperMixin to remove curses.

--- src/main/java/net/devoev/vanilla_cubed/mixin/EnchantmentHelperMixin.java
+++ src/main/java/net/devoev/vanilla_cubed/mixin/EnchantmentHelperMixin.java
@@ -2,13 +2,20 @@ package net.devoev.vanilla_cubed.mixin;
 
 import net.devoev.vanilla_cubed.util.ItemKt;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
 import java.util.Random;
 
+/**
+ * A mixin to modify the enchantment behavior of ancient gold.
+ */
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
 
@@ -18,5 +25,14 @@ public class EnchantmentHelperMixin {
     @ModifyVariable(method = "generateEnchantments", at = @At("HEAD"))
     private static boolean allowTreasureEnchantments(boolean treasureAllowed, Random random, ItemStack stack) {
         return ItemKt.isAncientGold(stack.getItem()) || treasureAllowed;
+    }
+
+    /**
+     * Filters out the curses, if the item is made of ancient gold.
+     */
+    @Inject(method = "generateEnchantments", at = @At("RETURN"), cancellable = true)
+    private static void removeCurses(Random random, ItemStack stack, int level, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> info) {
+        if (ItemKt.isAncientGold(stack.getItem()))
+            info.setReturnValue(info.getReturnValue().stream().filter(entry -> !entry.enchantment.isCursed()).toList());
     }
 }
