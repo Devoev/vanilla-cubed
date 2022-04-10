1ddc597243dd2286affb9ed8f4b3709ae3627760
Devin Balian <devinbalian@gmail.com>
2022-04-10 14:27:14 +0200

    Fixed LivingEntityFeatureRenderer.kt

--- src/main/kotlin/net/devoev/vanilla_cubed/client/render/entity/feature/LivingEntityFeatureRenderer.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/client/render/entity/feature/LivingEntityFeatureRenderer.kt
@@ -1,17 +1,33 @@
 package net.devoev.vanilla_cubed.client.render.entity.feature
 
 import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback
-import net.minecraft.client.render.entity.PlayerEntityRenderer
-import net.minecraft.client.render.entity.model.PlayerEntityModel
+import net.minecraft.client.render.entity.LivingEntityRenderer
+import net.minecraft.client.render.entity.feature.FeatureRenderer
+import net.minecraft.client.render.entity.model.EntityModel
+import net.minecraft.client.render.entity.model.EntityModelLoader
+import net.minecraft.entity.EntityType
+import net.minecraft.entity.LivingEntity
+import net.minecraft.util.Identifier
 
 
-object LivingEntityFeatureRenderer {
+object LivingEntityFeatureRenderer : MutableMap<Identifier,
+            (LivingEntityRenderer<*,*>, EntityModelLoader) -> FeatureRenderer<out LivingEntity, out EntityModel<out LivingEntity>>> by mutableMapOf() {
+
+    init {
+        this[EntityType.PLAYER] = { context, loader -> WingedDragonChestplateFeatureRenderer(context, loader) }
+    }
+
+    /**
+     * Adds the given [renderer] to this. Uses the "minecraft" namespace for the id.
+     */
+    operator fun set(type: EntityType<*>, renderer: (LivingEntityRenderer<*,*>, EntityModelLoader) -> FeatureRenderer<out LivingEntity, out EntityModel<out LivingEntity>>)
+        = set(EntityType.getId(type), renderer)
 
     fun init() {
         LivingEntityFeatureRendererRegistrationCallback.EVENT.register { entityType, livingEntityRenderer, registrationHelper, context ->
-            if (livingEntityRenderer.model is PlayerEntityModel<*>) {
-                registrationHelper.register(WingedDragonChestplateFeatureRenderer(PlayerEntityRenderer(context, false), context.modelLoader))
-            }
+            val id = EntityType.getId(entityType)
+            if (contains(id))
+                registrationHelper.register(this[id]!!(livingEntityRenderer, context.modelLoader))
         }
     }
 }
