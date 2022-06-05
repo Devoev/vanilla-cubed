ed36ea2fab3b77fea7166f174f7929fb8585f5b8
devin <devinbalian@gmail.com>
2022-06-05 14:40:57 +0200

    Added more helper methods to StatusEffectHelper.kt. Implemented applyHarmfulEffect and used it in AncientGoldSword

--- src/main/kotlin/net/devoev/vanilla_cubed/entity/effect/StatusEffectHelper.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/entity/effect/StatusEffectHelper.kt
@@ -2,7 +2,12 @@ package net.devoev.vanilla_cubed.entity.effect
 
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectCategory
+import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
+import java.time.Duration
+import kotlin.random.Random
+import kotlin.random.nextInt
+import kotlin.random.nextUInt
 import kotlin.reflect.full.staticProperties
 
 /**
@@ -19,4 +24,35 @@ object StatusEffectHelper {
     val harmful = all.filter { it.category == StatusEffectCategory.HARMFUL }
     val beneficial = all.filter { it.category == StatusEffectCategory.BENEFICIAL }
     val neutral = all.filter { it.category == StatusEffectCategory.NEUTRAL }
+
+    /**
+     * Returns a random [StatusEffectInstance] from the given [list].
+     * @param durationRange The range of values for the duration of the effect instance.
+     * @param amplifierRange The range of values for the amplifier of the effect instance.
+     */
+    private fun randomInstance(list: List<StatusEffect>, durationRange: IntRange, amplifierRange: IntRange): StatusEffectInstance {
+        val duration = Random.nextInt(durationRange)
+        val amplifier = Random.nextInt(amplifierRange)
+        return StatusEffectInstance(list.random(), duration, amplifier)
+    }
+
+    /**
+     * Returns a random [StatusEffectInstance].
+     */
+    fun random(durationRange: IntRange, amplifierRange: IntRange) = randomInstance(all, durationRange, amplifierRange)
+
+    /**
+     * Returns a random [harmful][StatusEffectCategory.HARMFUL] [StatusEffectInstance].
+     */
+    fun randomHarmful(durationRange: IntRange, amplifierRange: IntRange) = randomInstance(harmful, durationRange, amplifierRange)
+
+    /**
+     * Returns a random [beneficial][StatusEffectCategory.BENEFICIAL] [StatusEffectInstance].
+     */
+    fun randomBeneficial(durationRange: IntRange, amplifierRange: IntRange) = randomInstance(beneficial, durationRange, amplifierRange)
+
+    /**
+     * Returns a random [neutral][StatusEffectCategory.NEUTRAL] [StatusEffectInstance].
+     */
+    fun randomNeutral(durationRange: IntRange, amplifierRange: IntRange) = randomInstance(neutral, durationRange, amplifierRange)
 }
--- src/main/kotlin/net/devoev/vanilla_cubed/item/tool/AncientGoldTools.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/item/tool/AncientGoldTools.kt
@@ -1,16 +1,22 @@
 package net.devoev.vanilla_cubed.item.tool
 
+import net.devoev.vanilla_cubed.entity.effect.StatusEffectHelper
 import net.devoev.vanilla_cubed.item.ModItemGroup
 import net.devoev.vanilla_cubed.item.toSettings
 import net.minecraft.entity.LivingEntity
-import net.minecraft.entity.effect.StatusEffectUtil
-import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.item.ItemStack
+import kotlin.random.Random
 
 private val ancientGoldData = ToolData(material = ModToolMaterials.ANCIENT_GOLD, settings = ModItemGroup.TOOLS.toSettings())
 
+/**
+ * Applies a random harmful [StatusEffectInstance] to the given target with a 10% probability.
+ */
 private fun applyHarmfulEffect(target: LivingEntity?): Unit {
-    TODO("add StatusEffectHelper")
+    val effect = StatusEffectHelper.randomHarmful(100..200, 0..2)
+    if (Random.nextDouble() < 0.9 || effect.effectType.isInstant) return
+    target?.addStatusEffect(effect)
 }
 
 object AncientGoldSword : ModSwordItem(ancientGoldData) {
