9773495c9d5710e45a5f592029a34d89ebf2b205
devin <devinbalian@gmail.com>
2022-06-12 22:34:32 +0200

    Updated to 1.18.2

--- src/main/kotlin/net/devoev/vanilla_cubed/world/feature/ModPlacedFeatures.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/world/feature/ModPlacedFeatures.kt
@@ -2,11 +2,12 @@ package net.devoev.vanilla_cubed.world.feature
 
 import net.devoev.vanilla_cubed.util.RegistryManager
 import net.minecraft.util.registry.BuiltinRegistries
+import net.minecraft.util.registry.RegistryEntry
 import net.minecraft.world.gen.YOffset
-import net.minecraft.world.gen.decorator.*
 import net.minecraft.world.gen.feature.ConfiguredFeature
 import net.minecraft.world.gen.feature.OrePlacedFeatures
 import net.minecraft.world.gen.feature.PlacedFeature
+import net.minecraft.world.gen.placementmodifier.*
 
 /**
  * All placed features.
@@ -20,7 +21,7 @@ object ModPlacedFeatures : RegistryManager<PlacedFeature>(BuiltinRegistries.PLAC
      * Creates a [PlacedFeature] for ores.
      */
     private fun create(name: String, feature: ConfiguredFeature<*,*>, modifiers: List<PlacementModifier>)
-        = create(name, PlacedFeature({ feature }, modifiers))
+        = create(name, PlacedFeature(RegistryEntry.of(feature) , modifiers))
 
     /**
      * Helper methods for [OrePlacedFeatures].
