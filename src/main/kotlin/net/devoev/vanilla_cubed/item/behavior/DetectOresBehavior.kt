b6ccb7660e9b4896b82c0b628d69bd92ac59f7b0
devin <devinbalian@gmail.com>
2022-07-31 18:00:39 +0200

    Added playsound to DetectOresBehavior.kt

--- src/main/kotlin/net/devoev/vanilla_cubed/item/behavior/DetectOresBehavior.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/item/behavior/DetectOresBehavior.kt
@@ -2,18 +2,26 @@ package net.devoev.vanilla_cubed.item.behavior
 
 import net.devoev.vanilla_cubed.block.isIn
 import net.minecraft.item.Item
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 import net.minecraft.tag.BlockTags
 import net.minecraft.util.math.BlockPos
 
 /**
- * Detects nearby ores in the given [range].
+ * Detects nearby ores in the given [range] and plays a sound.
  */
 class DetectOresBehavior(private val range: Int) : PostMineBehavior<Item> {
 
     override fun apply(item: Item, params: PostMineBehavior.Params): Boolean {
         val blocks = BlockPos.iterateOutwards(params.pos, range, range, range).map { params.world?.getBlockState(it) }
         if (blocks.any { it?.isIn(ORES) == true })
-            println("Found ore!")
+            params.world?.playSound(
+                null,
+                params.pos,
+                SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME,
+                SoundCategory.AMBIENT,
+                25f,
+                1f)
         return false
     }
 
