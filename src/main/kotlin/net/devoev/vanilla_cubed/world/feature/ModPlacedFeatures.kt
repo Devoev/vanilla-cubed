3ff0ebe5a9993f32b8161353c387cb54c7ee03f9
Devin Balian <devinbalian@gmail.com>
2022-08-26 21:28:21 +0200

    Reduced enderite ore generation count from 15 to 3 Changed enderite ore advancement

--- src/main/kotlin/net/devoev/vanilla_cubed/world/feature/ModPlacedFeatures.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/world/feature/ModPlacedFeatures.kt
@@ -15,7 +15,7 @@ import net.minecraft.world.gen.placementmodifier.*
 object ModPlacedFeatures : RegistryManager<PlacedFeature>(BuiltinRegistries.PLACED_FEATURE) {
 
     val ENDERITE_ORE = create("enderite_ore", ModConfiguredFeatures.ENDERITE_ORE,
-        ORE.modifiersWithCount(15, HeightRangePlacementModifier.uniform(YOffset.aboveBottom(0), YOffset.aboveBottom(70))))
+        ORE.modifiersWithCount(3, HeightRangePlacementModifier.uniform(YOffset.aboveBottom(0), YOffset.aboveBottom(70))))
 
     /**
      * Creates a [PlacedFeature] for ores.
