f9a5760b4667a50df3012d189508e6387952b532
devin <devinbalian@gmail.com>
2022-08-28 18:03:15 +0200

    Fixed dimension teleportation of EnderitePowder.kt Added wait time to EnderitePowder.kt

--- src/main/kotlin/net/devoev/vanilla_cubed/item/EnderitePowder.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/item/EnderitePowder.kt
@@ -1,6 +1,8 @@
 package net.devoev.vanilla_cubed.item
 
 import net.devoev.vanilla_cubed.util.math.toVec3d
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions
+import net.minecraft.entity.EntityStatuses
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
@@ -10,6 +12,8 @@ import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.TeleportTarget
 import net.minecraft.world.World
 import kotlin.jvm.optionals.getOrNull
 
@@ -21,14 +25,17 @@ object EnderitePowder : Item(ModItemGroup.TOOLS.toSettings()) {
 
         if (stack == null || world !is ServerWorld || user !is ServerPlayerEntity) return TypedActionResult.pass(stack)
 
-        //TODO: Teleport to overworld
         val pos = user.spawnPointPosition ?: world.spawnPos
         var vec = PlayerEntity.findRespawnPosition(world, pos, user.limbAngle, false, user.isAlive).getOrNull()
 
         if (vec == null) vec = world.spawnPos.toVec3d()
+        val target = TeleportTarget(vec, Vec3d.ZERO, user.bodyYaw, user.pitch)
 
-        user.itemCooldownManager[this] = 20
-        user.teleport(vec.x, vec.y, vec.z, true)
+        user.itemCooldownManager[this] = 40
+        world.sendEntityStatus(user, EntityStatuses.ADD_PORTAL_PARTICLES)
+        Thread.sleep(2000)
+
+        FabricDimensions.teleport(user, world.server.overworld, target)
         world.playSound(null, user.blockPos, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 3f, -0.5f)
         stack.decrement(1)
         return TypedActionResult.success(stack, world.isClient)
