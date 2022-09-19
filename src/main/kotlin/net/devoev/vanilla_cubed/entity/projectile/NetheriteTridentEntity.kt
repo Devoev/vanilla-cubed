a63476a6cbe0c45afe8146d15b8c3dfc7e3d3763
devin <devinbalian@gmail.com>
2022-09-19 22:52:59 +0200

    Added item attraction to NetheriteTridentEntity.kt

--- src/main/kotlin/net/devoev/vanilla_cubed/entity/projectile/NetheriteTridentEntity.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/entity/projectile/NetheriteTridentEntity.kt
@@ -1,10 +1,34 @@
 package net.devoev.vanilla_cubed.entity.projectile
 
 import net.devoev.vanilla_cubed.entity.ModEntityTypes
+import net.devoev.vanilla_cubed.util.math.Vec3d
+import net.devoev.vanilla_cubed.util.math.minus
+import net.devoev.vanilla_cubed.util.math.plus
+import net.minecraft.entity.EntityType
+import net.minecraft.entity.ItemEntity
 import net.minecraft.entity.LivingEntity
 import net.minecraft.item.ItemStack
+import net.minecraft.predicate.entity.EntityPredicates
+import net.minecraft.util.hit.EntityHitResult
+import net.minecraft.util.math.Box
 import net.minecraft.world.World
 
 class NetheriteTridentEntity(world: World, owner: LivingEntity, stack: ItemStack)
     : ModTridentEntity(world, owner, stack, ModEntityTypes.NETHERITE_TRIDENT) {
+
+    private val items: MutableSet<ItemEntity> = mutableSetOf()
+
+    override fun tick() {
+        super.tick()
+
+        val range = 2.5
+
+        items.addAll(entityWorld.getEntitiesByType(
+            EntityType.ITEM,
+            Box(pos - Vec3d(range,range,range), pos + Vec3d(range,range,range)),
+            EntityPredicates.VALID_ENTITY
+        ))
+
+        items.forEach { it.setPosition(pos + Vec3d(0, 0.75, 0)) }
+    }
 }
