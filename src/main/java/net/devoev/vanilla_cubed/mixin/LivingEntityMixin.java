dc8d7024a276e81ed0a742344fc271cdf571a1d0
devin <devinbalian@gmail.com>
2022-10-07 16:24:14 +0200

    Updated imports Removed enderite teleporation ability

--- src/main/java/net/devoev/vanilla_cubed/mixin/LivingEntityMixin.java
+++ src/main/java/net/devoev/vanilla_cubed/mixin/LivingEntityMixin.java
@@ -1,14 +1,10 @@
 package net.devoev.vanilla_cubed.mixin;
 
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
---
+++ src/main/kotlin/net/devoev/vanilla_cubed/networking/EnderiteTeleportation.kt
@@ -0,0 +1,43 @@
+package net.devoev.vanilla_cubed.networking
+
+import net.devoev.vanilla_cubed.entity.falling
+import net.devoev.vanilla_cubed.entity.view
+import net.devoev.vanilla_cubed.util.math.plus
+import net.devoev.vanilla_cubed.util.math.times
+import net.devoev.vanilla_cubed.util.wearsEnderite
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking.PlayChannelHandler
+import net.minecraft.block.BlockState
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
+
+/**
+ * Teleports the player in the direction he is looking at,
+ * if he presses the jump key and wears a full set of enderite armor.
+ */
+@Deprecated("Enderite armor is not using the teleportation ability any more.")
+val ENDERITE_TELEPORTATION = PlayChannelHandler { _, player, _, _, _ ->
+    val stack = player.inventory.armor[0]
+
+    if (!player.falling || !player.wearsEnderite() || player.isFallFlying) {
+        stack.nbt?.putBoolean("enderite_jump_active", false)
+        return@PlayChannelHandler
+    }
+
+    if (stack.nbt?.getBoolean("enderite_jump_active") == true) return@PlayChannelHandler
+
+    player.run {
+        stack.nbt?.putBoolean("enderite_jump_active", true)
+        var dir = view * 5
+        var dest: Vec3d
+        var state: BlockState
+
+        do {
+            dest = pos + dir
+            state = world.getBlockState(BlockPos(dest.x, dest.y, dest.z))
+            dir *= 0.7
+            if (dir.length() < 0.1) return@PlayChannelHandler
+        } while (state.isOpaque)
+
+        teleport(dest.x, dest.y, dest.z)
+    }
+}
