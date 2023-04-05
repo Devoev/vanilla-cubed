542cfcfb064165b09e456908d821024a525f4498
Devin <devinbalian@gmail.com>
2023-04-05 22:20:45 +0200

    Injected modded beacon ticker

--- src/main/java/net/devoev/vanilla_cubed/mixin/BeaconBlockMixin.java
+++ src/main/java/net/devoev/vanilla_cubed/mixin/BeaconBlockMixin.java
@@ -4,7 +4,10 @@ import net.devoev.vanilla_cubed.block.entity.ModBeaconBlockEntity;
 import net.minecraft.block.BeaconBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,5 +22,13 @@ public class BeaconBlockMixin {
     @Inject(method = "createBlockEntity", at = @At("HEAD"), cancellable = true)
     private void useModdedBeaconEntity(BlockPos pos, BlockState state, CallbackInfoReturnable<BlockEntity> info) {
         info.setReturnValue(new ModBeaconBlockEntity(pos, state));
+    }
+
+    /**
+     * Replaces the ticker function with the modded beacons ticker.
+     */
+    @Inject(method = "getTicker", at = @At("HEAD"), cancellable = true)
+    private <T extends BlockEntity> void getTicker(World world, BlockState state, BlockEntityType<T> type, CallbackInfoReturnable<BlockEntityTicker<T>> info) {
+        info.setReturnValue(ModBeaconBlockEntity.Companion.ticker(type));
     }
 }
--- src/main/kotlin/net/devoev/vanilla_cubed/block/entity/ModBeaconBlockEntity.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/block/entity/ModBeaconBlockEntity.kt
@@ -2,12 +2,35 @@ package net.devoev.vanilla_cubed.block.entity
 
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BeaconBlockEntity
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.block.entity.BlockEntityTicker
+import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.screen.NamedScreenHandlerFactory
 import net.minecraft.text.Text
 import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 class ModBeaconBlockEntity(pos: BlockPos, state: BlockState) : BeaconBlockEntity(pos, state), NamedScreenHandlerFactory {
 
-
     override fun getDisplayName(): Text = Text.literal("Modded Beacon")
+
+    companion object {
+
+        /**
+         * Ticks the [ModBeaconBlockEntity].
+         */
+        private fun tick(world: World, pos: BlockPos, state: BlockState, blockEntity: BeaconBlockEntity) {
+            println("Lol!")
+            BeaconBlockEntity.tick(world, pos, state, blockEntity)
+        }
+
+        /**
+         * Provides the [tick] function of a [ModBeaconBlockEntity].
+         */
+        fun <T : BlockEntity> ticker(type: BlockEntityType<T>): BlockEntityTicker<T>? {
+            return if (type == BlockEntityType.BEACON)
+                BlockEntityTicker { world, pos, state, blockEntity -> tick(world, pos, state, blockEntity as BeaconBlockEntity) }
+            else null
+        }
+    }
 }
