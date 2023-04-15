338d18d7d40ad6c3dc7bfb779c4c879dd9ba209d
Devin <devinbalian@gmail.com>
2023-04-15 14:11:34 +0200

    Added ModBeaconScreenHandler.transferSlot (not properly working)

--- src/main/kotlin/net/devoev/vanilla_cubed/screen/ModBeaconScreenHandler.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/screen/ModBeaconScreenHandler.kt
@@ -4,8 +4,11 @@ import net.minecraft.block.Blocks
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
+import net.minecraft.screen.ArrayPropertyDelegate
+import net.minecraft.screen.PropertyDelegate
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.ScreenHandlerContext
+import net.minecraft.screen.slot.Slot
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
@@ -15,9 +18,45 @@ import net.minecraft.world.World
 class ModBeaconScreenHandler(syncId: Int, inventory: Inventory) : ScreenHandler(ModScreenHandlerTypes.MOD_BEACON, syncId) {
 
     private val context: ScreenHandlerContext = ScreenHandlerContext.EMPTY
+    private val propertyDelegate: PropertyDelegate = ArrayPropertyDelegate(3)
 
+    init {
+        checkDataCount(propertyDelegate, 3)
+        addProperties(propertyDelegate)
+        for (k in 0..3) {
+            for (l in 0..8) {
+                addSlot(Slot(inventory, l + k * 9 + 9, 36 + l * 18, 137 + k * 18))
+            }
+        }
+        for (k in 0..9) {
+            addSlot(Slot(inventory, k, 36 + k * 18, 195))
+        }
+    }
+
+    // FIXME: Wrong gui and inventory slots
     override fun transferSlot(player: PlayerEntity?, index: Int): ItemStack {
-        TODO("Not yet implemented")
+        var itemStack = ItemStack.EMPTY
+        val slot = slots[index]
+        if (slot.hasStack()) {
+            val itemStack2 = slot.stack
+            itemStack = itemStack2.copy()
+            if (index == 0) {
+                if (!insertItem(itemStack2, 1, 37, true)) {
+                    return ItemStack.EMPTY
+                }
+                slot.onQuickTransfer(itemStack2, itemStack)
+            }
+            if (itemStack2.isEmpty) {
+                slot.stack = ItemStack.EMPTY
+            } else {
+                slot.markDirty()
+            }
+            if (itemStack2.count == itemStack.count) {
+                return ItemStack.EMPTY
+            }
+            slot.onTakeItem(player, itemStack2)
+        }
+        return itemStack
     }
 
     override fun canUse(player: PlayerEntity?): Boolean {
