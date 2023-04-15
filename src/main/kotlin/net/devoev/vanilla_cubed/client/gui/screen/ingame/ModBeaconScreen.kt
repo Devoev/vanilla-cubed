7c98ecaa15e840d1228aee38a6fb93296a5549ad
Devin <devinbalian@gmail.com>
2023-04-15 14:00:58 +0200

    Injected mod_beacon.png screen Added ModBeaconScreenHandler.canUse

--- src/main/kotlin/net/devoev/vanilla_cubed/client/gui/screen/ingame/ModBeaconScreen.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/client/gui/screen/ingame/ModBeaconScreen.kt
@@ -1,7 +1,10 @@
 package net.devoev.vanilla_cubed.client.gui.screen.ingame
 
+import com.mojang.blaze3d.systems.RenderSystem
+import net.devoev.vanilla_cubed.VanillaCubed
 import net.devoev.vanilla_cubed.screen.ModBeaconScreenHandler
 import net.minecraft.client.gui.screen.ingame.HandledScreen
+import net.minecraft.client.render.GameRenderer
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.text.Text
@@ -10,6 +13,23 @@ class ModBeaconScreen(handler: ModBeaconScreenHandler, inventory: PlayerInventor
     : HandledScreen<ModBeaconScreenHandler>(handler, inventory, title) {
 
     override fun drawBackground(matrices: MatrixStack?, delta: Float, mouseX: Int, mouseY: Int) {
-        TODO("Not yet implemented")
+        RenderSystem.setShader { GameRenderer.getPositionTexShader() }
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f)
+        RenderSystem.setShaderTexture(0, TEXTURE)
+        val i = (width - backgroundWidth) / 2
+        val j = (height - backgroundHeight) / 2
+        this.drawTexture(matrices, i, j, 0, 0, backgroundWidth, backgroundHeight)
+//        itemRenderer.zOffset = 100.0f
+//        itemRenderer.renderInGuiWithOverrides(ItemStack(Items.NETHERITE_INGOT), i + 20, j + 109)
+//        itemRenderer.renderInGuiWithOverrides(ItemStack(Items.EMERALD), i + 41, j + 109)
+//        itemRenderer.renderInGuiWithOverrides(ItemStack(Items.DIAMOND), i + 41 + 22, j + 109)
+//        itemRenderer.renderInGuiWithOverrides(ItemStack(Items.GOLD_INGOT), i + 42 + 44, j + 109)
+//        itemRenderer.renderInGuiWithOverrides(ItemStack(Items.IRON_INGOT), i + 42 + 66, j + 109)
+//        itemRenderer.zOffset = 0.0f
+    }
+
+    companion object {
+
+        val TEXTURE = VanillaCubed.id("textures/gui/container/mod_beacon.png")
     }
 }
--- src/main/kotlin/net/devoev/vanilla_cubed/screen/ModBeaconScreenHandler.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/screen/ModBeaconScreenHandler.kt
@@ -1,26 +1,36 @@
 package net.devoev.vanilla_cubed.screen
 
+import net.minecraft.block.Blocks
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
-import net.minecraft.screen.BeaconScreenHandler
 import net.minecraft.screen.ScreenHandler
+import net.minecraft.screen.ScreenHandlerContext
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 /**
  * TODO: Implement functions similar to BeaconScreenHandler
  */
 class ModBeaconScreenHandler(syncId: Int, inventory: Inventory) : ScreenHandler(ModScreenHandlerTypes.MOD_BEACON, syncId) {
+
+    private val context: ScreenHandlerContext = ScreenHandlerContext.EMPTY
+
     override fun transferSlot(player: PlayerEntity?, index: Int): ItemStack {
         TODO("Not yet implemented")
     }
 
-    override fun close(player: PlayerEntity?) {
-        println("Closed modded beacon!")
-        super.close(player)
-    }
-
     override fun canUse(player: PlayerEntity?): Boolean {
-        TODO("Not yet implemented")
+        return context.get({ world: World, pos: BlockPos ->
+            if (!world.getBlockState(pos).isOf(Blocks.BEACON)) {
+                return@get false
+            }
+            player!!.squaredDistanceTo(
+                pos.x.toDouble() + 0.5,
+                pos.y.toDouble() + 0.5,
+                pos.z.toDouble() + 0.5
+            ) <= 64.0
+        }, true)
     }
 
 }
