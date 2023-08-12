b716ae5350ec56923d559b9894e6ce401fc99572
Devin <devinbalian@gmail.com>
2023-08-12 14:01:44 +0200

    Added disableEnvironmentalDamage implementation (still has bugs) Added FireBlockMixin

---
+++ src/main/java/net/devoev/vanilla_cubed/mixin/FireBlockMixin.java
@@ -0,0 +1,22 @@
+package net.devoev.vanilla_cubed.mixin;
+
+import net.devoev.vanilla_cubed.block.entity.beacon_upgrade.DisableEnvironmentalDamageKt;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FireBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FireBlock.class)
+public class FireBlockMixin {
+
+    @Inject(method = "scheduledTick", at = @At("HEAD"), cancellable = true)
+    private void disableFireTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (DisableEnvironmentalDamageKt.checkRange(pos))
+            ci.cancel();
+    }
+}
--- src/main/kotlin/net/devoev/vanilla_cubed/block/entity/beacon_upgrade/DisableEnvironmentalDamage.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/block/entity/beacon_upgrade/DisableEnvironmentalDamage.kt
@@ -1,15 +1,29 @@
 package net.devoev.vanilla_cubed.block.entity.beacon_upgrade
 
+import net.devoev.vanilla_cubed.util.math.toVec3d
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+
 /**
  * A beacon upgrade that prevents environmental damage like fire spread.
  */
 val disableEnvironmentalDamage = beaconUpgrade {
 
     activate {
-        println("active!")
+        activeRanges[it.pos] = it.beaconRange ?: return@activate
     }
 
     deactivate {
-        println("de-active!")
+        activeRanges.remove(it.pos)
     }
 }
+
+/**
+ * A map of active ranges by this upgrade.
+ */
+private val activeRanges: MutableMap<BlockPos, Box> = mutableMapOf()
+
+/**
+ * Whether the block pos is in range of an active [disableEnvironmentalDamage] upgrade.
+ */
+fun BlockPos.checkRange(): Boolean = activeRanges.values.any { this.toVec3d() in it }
