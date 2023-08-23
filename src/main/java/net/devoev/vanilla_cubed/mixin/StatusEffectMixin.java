7dfca660bcd10ac01fe8574ceb28bc2d1d0e719d
Devin <devinbalian@gmail.com>
2023-08-23 19:51:12 +0200

    Added new BeaconUpgrades Added StatusEffectMixin

---
+++ src/main/java/net/devoev/vanilla_cubed/mixin/StatusEffectMixin.java
@@ -0,0 +1,20 @@
+package net.devoev.vanilla_cubed.mixin;
+
+import net.devoev.vanilla_cubed.entity.effect.StatusEffectKt;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.effect.StatusEffect;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(StatusEffect.class)
+public class StatusEffectMixin {
+
+    @Inject(method = "onApplied", at=@At("RETURN"))
+    private void applyHealthBoost(LivingEntity entity, AttributeContainer attributes, int amplifier, CallbackInfo ci) {
+        StatusEffect effect = (StatusEffect) (Object) this;
+        StatusEffectKt.injectHealthBoost(effect, entity);
+    }
+}
---
+++ src/main/kotlin/net/devoev/vanilla_cubed/entity/effect/StatusEffect.kt
@@ -0,0 +1,17 @@
+package net.devoev.vanilla_cubed.entity.effect
+
+import net.devoev.vanilla_cubed.mixin.StatusEffectMixin
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.effect.StatusEffect
+import net.minecraft.entity.effect.StatusEffects
+
+/**
+ * Grants the player full health from the [StatusEffects.HEALTH_BOOST] effect instead of empty ones.
+ *
+ * @see StatusEffectMixin.applyHealthBoost
+ */
+fun injectHealthBoost(effect: StatusEffect, entity: LivingEntity) {
+    if (effect == StatusEffects.HEALTH_BOOST) {
+        entity.health += entity.maxHealth - 20
+    }
+}
