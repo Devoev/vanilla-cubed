3b4a21de499ef3b32cb682bb9c281175d2213bef
Devin <devinbalian@gmail.com>
2023-08-31 23:08:37 +0200

    Added BeaconBlock.kt and updated BeaconBlockMixin

--- src/main/java/net/devoev/vanilla_cubed/mixin/BeaconBlockMixin.java
+++ src/main/java/net/devoev/vanilla_cubed/mixin/BeaconBlockMixin.java
@@ -1,6 +1,6 @@
 package net.devoev.vanilla_cubed.mixin;
 
-import net.devoev.vanilla_cubed.block.entity.ModBeaconBlockEntity;
+import net.devoev.vanilla_cubed.block.BeaconBlockKt;
 import net.minecraft.block.BeaconBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
@@ -9,7 +9,6 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -25,50 +24,34 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class BeaconBlockMixin {
 
     /**
-     * Replaces the beacon block entity with the modded beacon block entity.
+     * @see BeaconBlockKt
      */
     @Inject(method = "createBlockEntity", at = @At("HEAD"), cancellable = true)
     private void useModdedBeaconEntity(BlockPos pos, BlockState state, CallbackInfoReturnable<BlockEntity> info) {
-        info.setReturnValue(new ModBeaconBlockEntity(pos, state));
+        BeaconBlockKt.createModBeaconBlockEntity(pos, state, info);
     }
 
     /**
-     * Replaces the ticker function with the modded beacons ticker.
+     * @see BeaconBlockKt
      */
     @Inject(method = "getTicker", at = @At("HEAD"), cancellable = true)
-    private <T extends BlockEntity> void useModdedBeaconTicker(World world, BlockState state, BlockEntityType<T> type, CallbackInfoReturnable<BlockEntityTicker<T>> info) {
-        info.setReturnValue(ModBeaconBlockEntity.Companion.ticker(type));
+    private <T extends BlockEntity> void modTicker(World world, BlockState state, BlockEntityType<T> type, CallbackInfoReturnable<BlockEntityTicker<T>> cir) {
+        BeaconBlockKt.modTicker(type, cir);
     }
 
     /**
-     * Replaces the beacon entity with the modded one.
+     * @see BeaconBlockKt
      */
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
-    private void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> info) {
-        if (world.isClient) {
-            info.setReturnValue(ActionResult.SUCCESS);
-        } else {
-            BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof ModBeaconBlockEntity) {
-                player.openHandledScreen((ModBeaconBlockEntity)blockEntity);
-                player.incrementStat(Stats.INTERACT_WITH_BEACON);
-            }
-
-            info.setReturnValue(ActionResult.CONSUME);
-        }
+    private void useModBeaconBlockEntity(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        BeaconBlockKt.useModBeaconBlockEntity(world, pos, player, cir);
     }
 
     /**
-     * Replaces the beacon entity with the modded one.
+     * @see BeaconBlockKt
      */
     @Inject(method = "onPlaced", at = @At("HEAD"), cancellable = true)
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack, CallbackInfo ci) {
-        if (itemStack.hasCustomName()) {
-            BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof ModBeaconBlockEntity) {
-                ((ModBeaconBlockEntity) blockEntity).setCustomName(itemStack.getName());
-                ci.cancel();
-            }
-        }
+    public void placeModBeaconBlockEntity(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack, CallbackInfo ci) {
+        BeaconBlockKt.placeModBeaconBlockEntity(world, pos, itemStack, ci);
     }
 }
---
+++ src/main/kotlin/net/devoev/vanilla_cubed/block/BeaconBlock.kt
@@ -0,0 +1,63 @@
+package net.devoev.vanilla_cubed.block
+
+import net.devoev.vanilla_cubed.block.entity.ModBeaconBlockEntity
+import net.devoev.vanilla_cubed.mixin.BeaconBlockMixin
+import net.minecraft.block.BeaconBlock
+import net.minecraft.block.BlockState
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.block.entity.BlockEntityTicker
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.stat.Stats
+import net.minecraft.util.ActionResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
+
+/**
+ * Replaces the beacon block entity in [BeaconBlock.createBlockEntity] with the modded beacon block entity.
+ */
+fun createModBeaconBlockEntity(pos: BlockPos, state: BlockState, cir: CallbackInfoReturnable<BlockEntity>) {
+    cir.returnValue = ModBeaconBlockEntity(pos, state)
+}
+
+/**
+ * Replaces the ticker function in [BeaconBlock.getTicker] with the modded beacons ticker.
+ * @see BeaconBlockMixin
+ */
+fun <T : BlockEntity> modTicker(type: BlockEntityType<T>, cir: CallbackInfoReturnable<BlockEntityTicker<T>>) {
+    cir.returnValue = ModBeaconBlockEntity.ticker(type)
+}
+
+/**
+ * Replaces the beacon entity with the modded one in the [BeaconBlock.onUse] method.
+ * @see BeaconBlockMixin.useModBeaconBlockEntity
+ */
+fun useModBeaconBlockEntity(world: World, pos: BlockPos, player: PlayerEntity, cir: CallbackInfoReturnable<ActionResult>) {
+    if (world.isClient) {
+        cir.returnValue = ActionResult.SUCCESS
+        return
+    }
+
+    val blockEntity = world.getBlockEntity(pos)
+    if (blockEntity is ModBeaconBlockEntity) {
+        player.openHandledScreen(blockEntity)
+        player.incrementStat(Stats.INTERACT_WITH_BEACON)
+    }
+    cir.returnValue = ActionResult.CONSUME
+}
+
+/**
+ * Replaces the beacon entity with the modded one in the [BeaconBlock.onPlaced] method.
+ * @see BeaconBlockMixin.placeModBeaconBlockEntity
+ */
+fun placeModBeaconBlockEntity(world: World, pos: BlockPos, stack: ItemStack, ci: CallbackInfo) {
+    if (stack.hasCustomName()) {
+        (world.getBlockEntity(pos) as? ModBeaconBlockEntity)?.let {
+            it.customName = stack.name
+            ci.cancel()
+        }
+    }
+}
