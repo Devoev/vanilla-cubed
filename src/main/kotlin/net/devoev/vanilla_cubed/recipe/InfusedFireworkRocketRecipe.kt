b0135e022d7e97a860a942221f14ed6bbf531a12
Devin <devinbalian@gmail.com>
2023-09-02 13:19:46 +0200

    updated InfusedFireworkRocketRecipe to be shaped

--- src/main/kotlin/net/devoev/vanilla_cubed/recipe/InfusedFireworkRocketRecipe.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/recipe/InfusedFireworkRocketRecipe.kt
@@ -1,7 +1,6 @@
 package net.devoev.vanilla_cubed.recipe
 
 import net.devoev.vanilla_cubed.inventory.toList
-import net.devoev.vanilla_cubed.inventory.toSet
 import net.devoev.vanilla_cubed.item.ModItems
 import net.devoev.vanilla_cubed.item.fireworksFlight
 import net.minecraft.inventory.CraftingInventory
@@ -18,17 +17,17 @@ import net.minecraft.world.World
 class InfusedFireworkRocketRecipe(id: Identifier) : SpecialCraftingRecipe(id) {
     override fun matches(inventory: CraftingInventory, world: World): Boolean {
         val stacks = inventory.toList()
-        val rockets = stacks.filter { it.isOf(Items.FIREWORK_ROCKET) }
-        val chunk = stacks.filter { it.isOf(ModItems.INFUSED_DRAGON_SCALE_CHUNK) }
+        val chunk = stacks[4]
+        val rockets = stacks - chunk
 
-        return rockets.size == 8
-                && chunk.size == 1
+        return rockets.all { it.isOf(Items.FIREWORK_ROCKET) }
+                && chunk.isOf(ModItems.INFUSED_DRAGON_SCALE_CHUNK)
                 && rockets.map { it.fireworksFlight }.toSet().size == 1
     }
 
     override fun craft(inventory: CraftingInventory): ItemStack {
         val res = ItemStack(ModItems.INFUSED_FIREWORK_ROCKET)
-        res.fireworksFlight = inventory.toSet().first().fireworksFlight
+        res.fireworksFlight = inventory.getStack(0).fireworksFlight
         return res
     }
 
