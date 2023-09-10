f7e63215fd46723dddb6a94f9e0b516d4abadfae
Devin <devinbalian@gmail.com>
2023-09-10 19:26:57 +0200

    extracted EnchantmentScreenHandlerMixin functions to GildedBook

--- src/main/java/net/devoev/vanilla_cubed/mixin/EnchantmentScreenHandlerMixin.java
+++ src/main/java/net/devoev/vanilla_cubed/mixin/EnchantmentScreenHandlerMixin.java
@@ -1,12 +1,7 @@
 package net.devoev.vanilla_cubed.mixin;
 
-import net.devoev.vanilla_cubed.item.ItemStackKt;
-import net.devoev.vanilla_cubed.item.ModItems;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
+import net.devoev.vanilla_cubed.item.GildedBookKt;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.screen.EnchantmentScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
@@ -16,8 +11,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Map;
 
 @Mixin(EnchantmentScreenHandler.class)
 public abstract class EnchantmentScreenHandlerMixin extends ScreenHandler {
@@ -31,22 +24,10 @@ public abstract class EnchantmentScreenHandlerMixin extends ScreenHandler {
     }
 
     /**
-     * Makes it possible to enchant gilded books, by replacing the enchanted ones with Enchanted Book items.
-     * Also sets the "gilded" NBT tag to true, for the custom texture to work.
+     * @see GildedBookKt
      */
     @Inject(method = "onButtonClick", at = @At("RETURN"))
-    public void replaceGildedBooksWithEnchantedBooks(CallbackInfoReturnable<Boolean> info) {
-        ItemStack stack = inventory.getStack(0);
-        if (!stack.isOf(ModItems.INSTANCE.getGILDED_BOOK())) return;
-
-        ItemStack res = new ItemStack(Items.ENCHANTED_BOOK);
-        ItemStackKt.setGilded(res, true);
-        if (stack.hasCustomName())
-            res.setCustomName(stack.getName());
-
-        Map<Enchantment, Integer> enchantments = EnchantmentHelper.fromNbt(stack.getEnchantments());
-        EnchantmentHelper.set(enchantments, res);
-
-        inventory.setStack(0, res);
+    public void enchantGildedBook(CallbackInfoReturnable<Boolean> cir) {
+        GildedBookKt.enchantGildedBook(inventory);
     }
 }
--- src/main/kotlin/net/devoev/vanilla_cubed/item/GildedBook.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/item/GildedBook.kt
@@ -1,9 +1,13 @@
 package net.devoev.vanilla_cubed.item
 
 import net.devoev.vanilla_cubed.mixin.EnchantmentHelperMixin
+import net.devoev.vanilla_cubed.mixin.EnchantmentScreenHandlerMixin
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.inventory.Inventory
 import net.minecraft.item.BookItem
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 
 class GildedBook : BookItem(FabricItemSettings()) {
 
@@ -16,4 +20,25 @@ class GildedBook : BookItem(FabricItemSettings()) {
  */
 fun addGildedBookEntry(isBook: Boolean, stack: ItemStack): Boolean {
     return stack.isOf(ModItems.GILDED_BOOK) || isBook;
+}
+
+/**
+ * Makes it possible to enchant gilded books, by replacing the enchanted ones with [Items.ENCHANTED_BOOK].
+ *
+ * Also sets the `gilded` NBT tag to `true`, for the custom texture to work.
+ *
+ * @see EnchantmentScreenHandlerMixin.enchantGildedBook
+ */
+fun enchantGildedBook(inventory: Inventory) {
+    val stack = inventory.getStack(0)
+    if (!stack.isOf(ModItems.GILDED_BOOK)) return
+
+    val res = ItemStack(Items.ENCHANTED_BOOK)
+    res.gilded = true
+    if (stack.hasCustomName()) res.setCustomName(stack.getName())
+
+    val enchantments = EnchantmentHelper.fromNbt(stack.getEnchantments())
+    EnchantmentHelper.set(enchantments, res)
+
+    inventory.setStack(0, res)
 }
