c2eccaf4c0650d6ea9344220c2682152626ee454
Devin <devinbalian@gmail.com>
2023-09-12 15:39:05 +0200

    implemented AmethystTridentEntity.onBlockHit

--- src/main/kotlin/net/devoev/vanilla_cubed/entity/projectile/AmethystTridentEntity.kt
+++ src/main/kotlin/net/devoev/vanilla_cubed/entity/projectile/AmethystTridentEntity.kt
@@ -1,7 +1,9 @@
 package net.devoev.vanilla_cubed.entity.projectile
 
 import net.devoev.vanilla_cubed.entity.ModEntityTypes
+import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.world.World
@@ -12,9 +14,30 @@ import net.minecraft.world.World
 class AmethystTridentEntity(world: World, owner: LivingEntity, stack: ItemStack)
     : ModTridentEntity(world, owner, stack, ModEntityTypes.AMETHYST_TRIDENT) {
 
+    private var blockHit: Boolean = false
+
     override fun onBlockHit(blockHitResult: BlockHitResult) {
         super.onBlockHit(blockHitResult)
-        // TODO: Update implementation
-        world.breakBlock(blockHitResult.blockPos, true, owner)
+
+        val pos = blockHitResult.blockPos
+        val blockState = world.getBlockState(pos)
+        val block = blockState.block
+
+        if (blockHit || block.hardness > 5 || block.hardness < 0) return
+
+        (owner as? PlayerEntity)?.let { owner ->
+            block.onBreak(world, pos, blockState, owner)
+            if (world.removeBlock(pos, false)) {
+                block.onBroken(world, pos, blockState)
+                block.afterBreak(world, owner, pos, blockState, world.getBlockEntity(pos), itemStack.copy())
+            }
+
+            // Post mine
+            if (!world.isClient && blockState.getHardness(world, pos) != 0.0f) {
+                itemStack.damage(1, owner) { it.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND) }
+            }
+
+            blockHit = true
+        }
     }
 }
